0f8acdc41efa8fe64331d85913c43947de6a6c23
mgiota <giota85@gmail.com>
2019-06-24 17:14:55 +0200

    4.7: Object Oriented programming

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { getSuggestions } from './api';
+import { InfoAlert } from './Alert';
 
 class CitySearch extends Component {
   state = {
@@ -10,7 +11,19 @@ class CitySearch extends Component {
 
   handleInputChanged = (event) => {
     const value = event.target.value;
-    getSuggestions(value).then(suggestions => this.setState({ suggestions }));
+    getSuggestions(value).then(suggestions => {
+      this.setState({ suggestions });
+      if (value && suggestions.length === 0) {
+        this.setState({
+          infoText: 'We can not find the city you are looking for. Please try another city',
+        });
+      } else {
+        this.setState({
+          infoText: '',
+        });
+      }
+    });
+
     this.setState({ query: value });
   }
 
@@ -22,6 +35,7 @@ class CitySearch extends Component {
   render() {
     return (
       <div className="CitySearch">
+        <InfoAlert text={this.state.infoText} />
         <input
           type="text"
           className="city"
