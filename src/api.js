285a86dd3366d6af1de2fb37cb4fecdeb062e9b2
mgiota <giota85@gmail.com>
2019-06-24 17:31:22 +0200

    4.8: PWA

--- src/api.js
+++ src/api.js
@@ -42,6 +42,11 @@ async function getEvents(lat, lon) {
         return mockEvents.events;
     }
 
+    if (!navigator.onLine) {
+      const events = localStorage.getItem('lastEvents');
+      return JSON.parse(events);
+    }
+
     const token = await getAccessToken();
     if (token) {
         let url = 'https://api.meetup.com/find/upcoming_events?&sign=true&photo-host=public'
@@ -51,7 +56,12 @@ async function getEvents(lat, lon) {
         url += '&lat=' + lat + '&lon=' + lon;
         }
         const result = await axios.get(url);
-        return result.data.events;
+        const events = result.data.events;
+        if (events.length) { // Check if the events exist
+          localStorage.setItem('lastEvents', JSON.stringify(events));
+        }
+
+        return events;
     }
     return [];
 }
