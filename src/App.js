226fa2861aec22b1dd108725e468cc32175b2674
mgiota <giota85@gmail.com>
2019-06-24 21:05:49 +0200

    4.9: charts

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import { getEvents } from './api';
+import moment from 'moment';
+import {
+  ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer
+} from 'recharts';
 
 class App extends Component {
   state = {
@@ -15,6 +19,16 @@ class App extends Component {
     this.updateEvents();
   }
 
+  countEventsOnADate = (date) => {
+    let count = 0;
+    for (let i = 0; i < this.state.events.length; i += 1) {
+      if (this.state.events[i].local_date === date) {
+        count += 1;
+      }
+    }
+    return count;
+  }
+
   updateEvents = (lat, lon) => {
     if (lat && lon) {
       getEvents(lat, lon).then(events => this.setState({ events, lat, lon }));
@@ -25,10 +39,44 @@ class App extends Component {
     }
   }
 
+  getData = () => {
+    const next7Days = []; // Create empty array for the next 7 days
+    const currentDate = moment(); // Today
+    // Loop 7 times for next 7 days
+    for (let i = 0; i < 7; i += 1) {
+      currentDate.add(1, 'days'); // Add 1 day to current date, currentDate changes
+      const dateString = currentDate.format('YYYY-MM-DD'); // Format the date
+      // Use the countEventsOnADate function to count #events on this date
+      const count = this.countEventsOnADate(dateString);
+      next7Days.push({ date: dateString, number: count }); // Add this date and number to the list
+    }
+    return next7Days;
+  }
+
   render() {
+    const data = this.getData();
+    console.log(data)
     return (
       <div className="App">
         <CitySearch updateEvents={this.updateEvents}/>
+        <ResponsiveContainer height={400}>
+          <ScatterChart
+            width={400}
+            height={400}
+            margin={{
+              top: 20, right: 20, bottom: 20, left: 20,
+            }}
+          >
+            <CartesianGrid />
+            <XAxis type="category" dataKey="date" name="date" />
+            <YAxis type="number" dataKey="number" name="number of events" allowDecimals={false} />
+            <XAxis type="number" dataKey="x" name="stature" unit="cm" />
+            <YAxis type="number" dataKey="y" name="weight" unit="kg" />
+            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+            <Scatter name="A school" data={this.getData()} fill="#8884d8" />
+          </ScatterChart>
+        </ResponsiveContainer>
+
         <EventList events={this.state.events} />
       </div>
     );
--- src/Event.js
+++ src/Event.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { PieChart, Pie, Tooltip, Cell, Legend } from 'recharts';
 
 class Event extends Component {
     state = {
@@ -7,6 +8,27 @@ class Event extends Component {
 
     handleClick = () => {
         this.setState({ showDetails: !this.state.showDetails })
+    }
+
+    getData() {
+       const event = this.props.event;
+       console.log(event);
+       let data = [];
+       if (event.rsvp_limit) {
+           const free = event.rsvp_limit - event.yes_rsvp_count;
+           data.push(
+                {
+                    name: "reservations",
+                    value: event.yes_rsvp_count
+                },
+                {
+                    name: "free",
+                    value: free
+
+                }
+            );
+       }
+       return data;
     }
 
     render() {
@@ -18,12 +40,23 @@ class Event extends Component {
         if (event.venue) {
             address = event.venue.name + ", " + event.venue.address_1 + ", " + event.venue.city + ", " + event.venue.localized_country_name
         };
+        const data = this.getData();
+        const colors = ["#9c2222", "#ccc"];
         return (
             <div className="Event">
                 <div className="eventDate">{event.local_time} - {event.local_date}</div>
                 <div className="eventName">{event.name} </div>
                 <div className="groupName">{event.group.name} </div>
                 <div className="going">{event.yes_rsvp_count} people are going</div>
+                { event.rsvp_limit &&
+                    <PieChart height={140} width={400}>
+                        <Pie data={data} cx={75} cy={75} innerRadius={30} outerRadius={40} fill="#8884d8" label>{
+                            data.map((entry, index) => <Cell fill={colors[index % colors.length]} />)}
+                        </Pie>
+                        <Legend iconSize={10} layout="vertical" verticalAlign="middle" align="right" />
+                        <Tooltip />
+                    </PieChart>
+                }
                 <div className={details}>
                     <p className="address" >{address} </p>
                     <div dangerouslySetInnerHTML={{ __html: event.description }} />
